d07b0f55b95b99c0a54ad30997b02792752de29a
fishedee <306766045@qq.com>
2023-08-02 20:04:22 +0800

    Updates

--- src/main/java/com/fishedee/web_boost/WebBoostMvcConfig.java
+++ src/main/java/com/fishedee/web_boost/WebBoostMvcConfig.java
@@ -1,10 +1,15 @@
 package com.fishedee.web_boost;
 
 import com.fishedee.web_boost.autoconfig.WebBoostProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.annotation.PostConstruct;
@@ -14,6 +19,7 @@ import java.util.List;
 /**
  * Created by fish on 2021/5/31.
  */
+@Slf4j
 public class WebBoostMvcConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -28,22 +34,5 @@ public class WebBoostMvcConfig implements WebMvcConfigurer {
             return;
         }
         registry.addInterceptor(logTimeHandlerInterceptor);
-    }
-
-    @Autowired
-    private RequestMappingHandlerAdapter adapter;
-
-    @Autowired
-    private WebBoostRequestResolver requestModelArgumentResolver;
-
-    @PostConstruct
-    public void injectSelfMethodArgumentResolver() {
-        if( webBoostProperties.isEnable() == false ) {
-            return;
-        }
-        List<HandlerMethodArgumentResolver> argumentResolvers = new ArrayList<>();
-        argumentResolvers.add(requestModelArgumentResolver);
-        argumentResolvers.addAll(adapter.getArgumentResolvers());
-        adapter.setArgumentResolvers(argumentResolvers);
     }
 }
